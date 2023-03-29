81d48eabc724b0247a8b4039e2e8519de476a90e
Sree140403 <128037721+Sree140403@users.noreply.github.com>
2023-03-29 15:06:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.js'
+import Daythree from './Daythree.js';
+function App(){
+  
+  return(
+    <div className="App">
+    
+    <Daythree/>
+      
+   
+
+      
+    </div>
+
+  );
+}
+
+export default App
+
+
+
